9c03b4cd9f38405e7b7268e3be8e1b86ef301a9c
Christian Krebs <christian.krebs.pi@gmail.com>
2019-04-12 19:20:25 +0200

    playing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { Subject } from 'rxjs';
-import { shareReplay, startWith } from 'rxjs/operators';
+import { shareReplay, startWith, map } from 'rxjs/operators';
 
 const console = {
   log: (...args) =>
@@ -13,14 +13,19 @@ const dispatchAction = action => action$.next(action);
 let count = 0;
 setInterval(() => dispatchAction(count++), 1000);
 
+let startCount = 1;
+
+const START = Symbol('START');
+
 const test$ = action$.pipe(
-  startWith('hello'),
+  startWith(START),
+  map(i => (i === START ? `start count ${startCount++}` : i)),
   shareReplay({ refCount: true, bufferSize: 1 })
 );
 
 const t1 = (message, name, duration) => {
   console.log(message);
-  const subscription = test$.subscribe(m => console.log(name, m));
+  const subscription = test$.subscribe(m => console.log(`${name}: ${m}`));
   setTimeout(() => subscription.unsubscribe(), duration);
 };
 
